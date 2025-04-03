32c202110667192a8e6c9da14128721a928b171f
PhilipeF <philipefdev@gmail.com>
2025-04-03 09:40:50 -0300

    Q.A do componente Header - ok

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -19,15 +19,15 @@ export const HeaderContainer = styled.div`
 
         color: ${props => props.theme['gray-100']};
         
-        border-bottom: 3px solid transparent;        
+        border-bottom: 3px solid transparent;   
+        
+        &.active {
+            color: ${props => props.theme['green-500']};
+        }
     }
 
     a:hover {
         border-bottom: 3px solid ${props => props.theme['green-500']};
-    }
-
-    &.active {
-        color: ${props => props.theme['green-500']};
-    }
+    }  
 
 `

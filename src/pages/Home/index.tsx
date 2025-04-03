cbef0425d6393f42cf8a221673196ec73b1b25f5
PhilipeF <philipefdev@gmail.com>
2025-04-03 15:28:17 -0300

    Component Home 80% - ok

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,28 @@
-export function Home () {
+import { Play } from "phosphor-react";
+import { ContDownContainer, FormContainer, HomeContainer, Separator } from "./styles";
+
+export function Home() {
     return (
-        <h1>Home</h1>
+        <HomeContainer>
+            <form action="">
+                <FormContainer>
+                    <label htmlFor="task">Vou trabalhar em</label>
+                    <input type="text" id="task" placeholder="Dê nome para o seu projeto" />
+
+                    <label htmlFor="minutesAmount">durante</label>
+                    <input type="number" id="minutesAmount" placeholder="- 00 +" />
+                    <span>minutos.</span>
+                </FormContainer>
+
+                <ContDownContainer>
+                    <span>0</span>
+                    <span>0</span>
+                    <Separator>:</Separator>
+                    <span>0</span> 
+                    <span>0</span>
+                </ContDownContainer>
+                <button> <Play size={24} /> Começar </button>
+            </form>
+        </HomeContainer >
     )
 }
---
+++ src/pages/Home/styles.ts
@@ -0,0 +1,55 @@
+import styled from "styled-components";
+
+export const HomeContainer = styled.main`
+    flex: 1;
+
+    display: flex; 
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    form {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        gap: 3.5rem;
+    }      
+`
+
+export const FormContainer = styled.div`     
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 0.5rem;
+    color: ${props => props.theme['gray-100']};
+    font-size: 1.125rem;
+    font-weight: bold;
+    flex-wrap: wrap;
+`;
+
+export const ContDownContainer = styled.div`
+    font-family: 'Roboto Mono', sans-serif;
+    font-weight: bold;
+    font-size: 10rem;
+    color: ${props => props.theme['gray-100']};
+
+    display: flex;
+    gap: 1rem;
+
+    span {
+        padding: 2rem 1rem;
+        background-color: ${props => props.theme['gray-700']};
+        border-radius: 8px;
+    }
+`;
+
+export const Separator = styled.div`
+    padding: 2rem 0;
+    color: ${props => props.theme['green-500']};
+    display: flex;
+    justify-content: center;
+    
+
+
+`

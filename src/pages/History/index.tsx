8348a241fb3c75082586707f00ef150767ac2a1e
PhilipeF <philipefdev@gmail.com>
2025-04-07 16:26:41 -0300

    Tabela da pagina history 70% - ok

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,5 +1,42 @@
-export function History () {
+import { HistoryContainer, HistoryList } from "./style";
+
+export function History() {
     return (
-        <h1>History</h1>
+        <HistoryContainer>
+            <h1>Meu histórico</h1>
+            <HistoryList>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Tarefa</th>
+                            <th>Duração</th>
+                            <th>Início</th>
+                            <th>Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Conserto de débitos técnicos </td>
+                            <td>25 minutos</td>
+                            <td>Há cerca de 2 meses</td>
+                            <td>Em andamento</td>
+                        </tr>
+                        <tr>
+                            <td>Conserto de débitos técnicos </td>
+                            <td>25 minutos</td>
+                            <td>Há cerca de 2 meses</td>
+                            <td>Em andamento</td>
+                        </tr>
+                        <tr>
+                            <td>Conserto de débitos técnicos </td>
+                            <td>25 minutos</td>
+                            <td>Há cerca de 2 meses</td>
+                            <td>Em andamento</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </HistoryList>
+
+        </HistoryContainer>
     )
 }

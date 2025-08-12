7a218f1e102976bb68dfea9786a24d44b72ebe8b
PhilipeF <philipefdev@gmail.com>
2025-08-12 16:38:25 -0300

    Alterando o if para swtiche para ficar mais legível a funcionalidade

--- src/context/CyclesContext.tsx
+++ src/context/CyclesContext.tsx
@@ -40,103 +40,101 @@ export function CyclesContextProvider({
     children
 }: CycleContextProviderProps) {
     const [cyclesState, dispatch] = useReducer((state: CyclesState, action: any) => {
-        if (action.type === "ADD_NEW_CYCLE") {
-            return {
-                ...state,
-                cycles: [...state.cycles, action.payload.newCycle],
-                activeCycleId: action.payload.newCycle.id,
-            }
+        switch (action.type) {
+            case "ADD_NEW_CYCLE":
+                return {
+                    ...state,
+                    cycles: [...state.cycles, action.payload.newCycle],
+                    activeCycleId: action.payload.newCycle.id
+                };
+            case "INTERRUPT_CURRENT_CYCLE":
+                return {
+                    ...state,
+                    cycles: state.cycles.map((cycle) => {
+                        if (cycle.id === state.activeCycleId) {
+                            return { ...cycle, interruputDate: new Date() }
+                        } else {
+                            return cycle;
+                        }
+                    }),
+                    activeCycleId: null
+                };
+            case "MARK_CURRENTY_CYCLE_FINISHED":
+                return {
+                    ...state,
+                    cycles: state.cycles.map((cycle) => {
+                        if (cycle.id === state.activeCycleId) {
+                            return { ...cycle, finishDate: new Date() }
+                        } else {
+                            return cycle;
+                        }
+                    }),
+                    activeCycleId: null
+                };
+            default:
+                return state
         }
-
-        if (action.type === "INTERRUPT_CURRENT_CYCLE") {
-            return {
-                ...state,
-                cycles: state.cycles.map((cycle) => {
-                    if (cycle.id === state.activeCycleId) {
-                        return { ...cycle, interruputDate: new Date() }
-                    } else {
-                        return cycle;
-                    }
-                }),
-                activeCycleId: null
-            }
-        }
-
-        if (action.type === "MARK_CURRENTY_CYCLE_FINISHED") {
-            return {
-                ...state,
-                cycles: state.cycles.map((cycle) => {
-                    if (cycle.id === state.activeCycleId) {
-                        return { ...cycle, finishDate: new Date() }
-                    } else {
-                        return cycle;
-                    }
-                }),
-                activeCycleId: null
-            }
-        }
-
-        return state
     }, {
         cycles: [],
-        activeCycleId: null
-    });
+        activeCycleId: null  
+    })
 
-    const [amountSecondsPassed, setAmountSecondsPassed] = useState(0);
+const [amountSecondsPassed, setAmountSecondsPassed] = useState(0);
 
-    const { cycles, activeCycleId } = cyclesState;
-    const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId);
+const { cycles, activeCycleId } = cyclesState;
+const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId);
 
-    function markCurrentCycleAsFinished() {
-        dispatch({
-            type: 'MARK_CURRENT_CYCLE_AS_FINISHED',
-            payload: ''
-        })
+function markCurrentCycleAsFinished() {
+    dispatch({
+        type: 'MARK_CURRENT_CYCLE_AS_FINISHED',
+        payload: ''
+    })
+}
+
+function setSecondsPassed(seconds: number) {
+    setAmountSecondsPassed(seconds);
+}
+
+function interruptCurrentCycle() {
+    dispatch({
+        type: 'INTERRUPT_CURRENT_CYCLE',
+        payload: ''
+    })
+}
+
+function createNewCycle(data: CreateCycleDate) {
+    const id = String(new Date().getTime())
+
+    const newCycle: Cycle = {
+        id,
+        task: data.task,
+        minutesAmount: data.minutesAmount,
+        startDate: new Date()
     }
 
-    function setSecondsPassed(seconds: number) {
-        setAmountSecondsPassed(seconds);
-    }
-
-    function interruptCurrentCycle() {
-        dispatch({
-            type: 'INTERRUPT_CURRENT_CYCLE',
-        })
-    }
-
-    function createNewCycle(data: CreateCycleDate) {
-        const id = String(new Date().getTime())
-
-        const newCycle: Cycle = {
-            id,
-            task: data.task,
-            minutesAmount: data.minutesAmount,
-            startDate: new Date()
+    setAmountSecondsPassed(0);
+    dispatch({
+        type: 'ADD_NEW_CYCLE',
+        payload: {
+            newCycle
         }
+    })
+}
 
-        setAmountSecondsPassed(0);
-        dispatch({
-            type: 'ADD_NEW_CYCLE',
-            payload: {
-                newCycle
-            }
-        })
-    }
-
-    return (
-        <CyclesContext.Provider
-            value={{
-                cycles,
-                activeCycle,
-                activeCycleId,
-                amountSecondsPassed,
-                markCurrentCycleAsFinished,
-                setSecondsPassed,
-                createNewCycle,
-                interruptCurrentCycle
-            }}
-        >
-            {children}
-        </CyclesContext.Provider>
-    )
+return (
+    <CyclesContext.Provider
+        value={{
+            cycles,
+            activeCycle,
+            activeCycleId,
+            amountSecondsPassed,
+            markCurrentCycleAsFinished,
+            setSecondsPassed,
+            createNewCycle,
+            interruptCurrentCycle
+        }}
+    >
+        {children}
+    </CyclesContext.Provider>
+)
 }
